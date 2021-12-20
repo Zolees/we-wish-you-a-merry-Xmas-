91361efc68f0cbfd404bd6c0898287daf841f4e0
linux32532 <94577827+linux32532@users.noreply.github.com>
2021-12-20 15:48:56 -0800

    Add files via upload

---
+++ xmas bot.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 20 13:41:19 2021
+
+@author: bubba
+"""
+
+#python christmas bot 
+
+from win32com.client import Dispatch
+
+speak = Dispatch("SAPI.SpVoice").Speak
+
+speak("we miss you a merry Christmas and a happy new year Good tidings to you wherever Good tidings for Christmas and a Happy New Year! we wish you a merry christmas we wish you a merry christmas we wish you a merry christmas and a happy new year! Oh, bring us a figgy pudding Oh, bring us a figgy pudding and a cup of cheer Good tidings to you wherever you are Good tidings for Christmas and a Happy New Year! Oh, bring us a figgy pudding Oh, bring us a figgy pudding Oh, bring us a figgy pudding and bring a cup of good cheer Good tidings to you wherever you are Good tidings for Christmas and a Happy New Year! we wish you a merry christmas we wish you a merry christmas we wish you a merry christmas and a happy new year!") 
